13853adcf862d769fb834e959117acc0fd50d80d
isom3400whichan <whichan@connect.ust.hk>
2025-12-17 16:46:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import streamlit as st
 import pandas as pd
 import os
 
-# Get the current working directory
-current_directory = os.getcwd()
-# Define the file path
-file_path = os.path.join(current_directory, 'winequality-red.csv')
+st.title("Retail Business Dashboard")
 
-# Read the CSV file into a DataFrame
-df = pd.read_csv(file_path, delimiter=';')
+st.header("Manager Input Section")
 
-# Display the DataFrame in an interactive table
-st.write("Wine Quality Data")
-st.dataframe(df)
+st.write("Please enter the monthly sales target and select the region.")
+
+st.number_input("Enter Monthly Sales Target (in USD):",
+                min_value = 0,
+                max_value = 100000000
+                value =50000)
+
+st.selectbox("Select Region:", 
+             ["North", "South", "East", "West"]
+
+if st.button("Submit"):
+              st.button("Submitted!")
+
+st.success("Dashboard updated successfully!")
