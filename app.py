4eef46d272337d38ed1173e3a1adfc2689694779
isom3400whichan <whichan@connect.ust.hk>
2025-12-17 16:46:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Please enter the monthly sales target and select the region.")
 
 st.number_input("Enter Monthly Sales Target (in USD):",
                 min_value = 0,
-                max_value = 100000000
+                max_value = 100000000,
                 value =50000)
 
 st.selectbox("Select Region:", 
