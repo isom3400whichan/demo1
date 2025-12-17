5da06f099f53f179bdd6dab73af19a9f303003b8
isom3400whichan <whichan@connect.ust.hk>
2025-12-17 10:07:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
 st.title("Business Dashboard with Streamlit Layouts")
-
 st.write("Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard.")
 
